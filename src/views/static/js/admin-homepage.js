9cc76c5e791fda3a81ec1cbfcf786cfa44b88bbe
Gagan <Gagan@192.168.1.9>
2019-03-20 22:15:35 +0000

    Admin page Tab2(Form2 Modal Started)

--- src/views/static/js/admin-homepage.js
+++ src/views/static/js/admin-homepage.js
@@ -8,7 +8,8 @@
 
 $(document).ready(function () {
     
-    client_request_table();
+    form1_request_table();
+    form2_request_table();
     
 });
 
@@ -16,7 +17,7 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-function client_request_table()
+function form1_request_table()
 { 
     var LCT = $('#form1_client_table').DataTable();
     
@@ -34,7 +35,7 @@ function client_request_table()
              //   var id = jsonObj[i]["CLIENT_ID"];
                
                 LCT.row.add([
-                    "<input type='radio' name='optradio' onclick=\"(function(){Openclientinfo(\'"+i+"\')})()\">",
+                    "<input type='radio' name='optradio' onclick=\"(function(){Openclient_form1(\'"+i+"\')})()\">",
 //                    activation_date,
                    i,// jsonObj[i]["CLIENT_ID"],
                    i,// jsonObj[i]["NAME"],
@@ -50,7 +51,8 @@ function client_request_table()
     });
 }
 
-function Openclientinfo(id)
+
+function Openclient_form1(id)
 {
     
     var reference_id = id;
@@ -64,6 +66,59 @@ function Openclientinfo(id)
         success : function(responseText) {
                 
         $('#form1_request_modal').modal('show');   
+        }
+    });
+ }
+
+
+function form2_request_table()
+{ 
+    var LCT = $('#form2_client_table').DataTable();
+    
+    $.ajax({
+        url : '',
+        data:{
+                
+            },
+        success : function(responseText) {
+        
+         //   var jsonObj = JSON.parse(responseText);
+         //   var arrayLength = jsonObj.length;
+            for (var i=0;i<10;i++)
+            {  
+             //   var id = jsonObj[i]["CLIENT_ID"];
+               
+                LCT.row.add([
+                    "<input type='radio' name='optradio' onclick=\"(function(){Openclient_form2(\'"+i+"\')})()\">",
+//                    activation_date,
+                   i,// jsonObj[i]["CLIENT_ID"],
+                   i,// jsonObj[i]["NAME"],
+                   i,// jsonObj[i]["PBUILDINGNAME"],
+                   i,// jsonObj[i]["STREET_NAME"],
+                   i,
+                   i,//  jsonObj[i]["ACTIVATION_CODE"],
+                    "Pending"
+                 //   parseInt(jsonObj[i]["PERMISSIONS"]) === 1 ? 'All Access' : 'Read Only',
+                ]).draw(false);
+            }//onclick=\"(function(){UsersandPermission_edit_button(\'"+user_id+"\')})()\"
+        }
+    });
+}
+
+function Openclient_form2(id)
+{
+    
+    var reference_id = id;
+    $("#ref_no").val(reference_id);
+    
+    //ajax call
+    $.ajax({
+        url : '',
+        data:{
+            },
+        success : function(responseText) {
+                
+        $('#form2_request_modal').modal('show');   
         }
     });
  }
