c25fd7564ce00dea491bdcfee17036561955a4b1
Gagan <Gagan@192.168.1.9>
2019-04-30 21:12:24 +0100

    revert changes

--- src/views/static/js/user_dashboard.js
+++ src/views/static/js/user_dashboard.js
@@ -22,13 +22,69 @@ function application_form(formNumber, formUID) {
 
 $(document).ready(function(){
   $('#user_homepage a').trigger('onclick');
-  
+  $('#overlay1').show();
+  $('#img').show();
+  $.get({
+    url : appLocation + '/user/getForms',
+    data : {},
+    success : function(responseData) {
+        $('#img').hide();
+        $('#overlay1').hide();
+        console.log('all user forms current', responseData);
+        // update the dashboard table here to pick all the data.
+        // if no forms the responseData = [] empty array
+        var CAT = $('#client_application_table').DataTable();
+        if (responseData.length) {
+            responseData.forEach(formResponse => {
+
+                var date = new Date(formResponse.createDate).toString();
+                var response_date = date.substr(0,28);
+
+                var application_type 
+                if(formResponse.formNumber === 1)
+                {
+                    application_type = "Visa extension"; 
+                }
+                else if(formResponse.formNumber === 2){
+                    application_type = "Entry visa application";
+                }
+
+
+                CAT.row.add([
+                    response_date,
+                    formResponse.formUID,
+                    application_type,
+                    getStatusFromCode(formResponse.status),
+                    "<label onclick=\"(function(){application_form("+formResponse.formNumber+",\'"+formResponse.formUID+"\')})()\" \n\
+                    style='cursor:pointer;color: #4d79ff;'><u>View Application</u></label>",
+                    // "<label onclick=\"(function(){OpenDevicePage(\'"+i+"\')})()\" \n\
+                    // style='background-color:"+""+";border-radius:10px; color: grey; cursor: pointer; padding: 5px 10px;' type='button'>\n\
+                    //     Property Profile\n\
+                    // </label>"
+    
+    
+                ]).draw(false);
+            });
+        }
+    },
+    error: function(xhr) {
+        $('#img').hide();
+        $('#overlay1').hide();
+        swal({
+            title: "Server Error",
+            text: "It seems like the server is down or under maintainance, please check back later.",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        });
+    },
+});
 });
 
 function change_content(identifier)
 {
     if(identifier==='user-home')
-    {   alert("aec");
+    {   
         $("#change_content").load(appLocation + '/user/homepage');
     } 
     else if(identifier==='contact')
